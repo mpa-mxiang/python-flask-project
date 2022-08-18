affaf67528c50f735af91d915d6cb1bf72a34ddd
Jiachun Xiang <jcxiang1230@gmail.com>
2022-08-18 18:04:47 -0400

    update

--- views.py
+++ views.py
@@ -1,6 +1,29 @@
-from flask import Flask
+from flask import Blueprint, render_template, jsonify, redirect, url_for
 
-app = Flask(__name__)
+views = Blueprint(__name__, "views")
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+
+@views.route("/")
+def home():
+    return render_template("index.html", name="Lily", age=21)
+
+
+@views.route("/profile/<username>")
+def profile(username):
+    return render_template("profile.html", name=username)
+
+
+@views.route("/json")
+def get_json():
+    return jsonify({'name': 'Lily', 'coolness': 10})
+
+
+@views.route("/data")
+def get_data():
+    data = request.json
+    return jsonify(data)
+
+
+@views.route("/go-to-home")
+def go_to_home():
+    return redirect(url_for("home"))
